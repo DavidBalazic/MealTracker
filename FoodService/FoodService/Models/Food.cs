e8be1eed311860a482807e9d701f4e46d5fb5ee0
DavidBalazic <davidbalazic31@hotmail.com>
2024-12-15 15:23:00 +0100

    feat: added food service models

---
+++ FoodService/FoodService/Models/Food.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FoodService.Models
+{
+    public class Food
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("calories")]
+        public double Calories { get; set; }
+
+        [BsonElement("protein")]
+        public double Protein { get; set; }
+
+        [BsonElement("carbohydrates")]
+        public double Carbohydrates { get; set; }
+
+        [BsonElement("fat")]
+        public double Fat { get; set; }
+
+        [BsonElement("servingSize")]
+        public string ServingSize { get; set; }
+
+        [BsonElement("allergens")]
+        public List<string> Allergens { get; set; }
+    }
+}
