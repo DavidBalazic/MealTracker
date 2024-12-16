a2a4e65a0595d840e299f545bf0a643079cf2689
DavidBalazic <davidbalazic31@hotmail.com>
2024-12-16 11:11:09 +0100

    feat: added recipe service

---
+++ RecipeService/RecipeService/Services/RecipesService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using RecipeService.Models;
+
+namespace RecipeService.Services
+{
+    public class RecipesService
+    {
+        private readonly IMongoCollection<Recipe> _recipesCollection;
+
+        public RecipesService(
+        IOptions<RecipeServiceDatabaseSettings> recipeServiceDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                recipeServiceDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                recipeServiceDatabaseSettings.Value.DatabaseName);
+
+            _recipesCollection = mongoDatabase.GetCollection<Recipe>(
+                recipeServiceDatabaseSettings.Value.RecipeCollectionName);
+        }
+
+        public async Task<List<Recipe>> GetAsync() => await _recipesCollection.Find(_ => true).ToListAsync();
+        public async Task<Recipe> GetAsync(string id) => await _recipesCollection.Find(p => p.Id == id).FirstOrDefaultAsync();
+        public async Task CreateAsync(Recipe recipe) => await _recipesCollection.InsertOneAsync(recipe);
+        public async Task UpdateAsync(string id, Recipe updateRecipe) => await _recipesCollection.ReplaceOneAsync(p => p.Id == id, updateRecipe);
+        public async Task DeleteAsync(string id) => await _recipesCollection.DeleteOneAsync(p => p.Id == id);
+    }
+}
