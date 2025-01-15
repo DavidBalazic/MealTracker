ac2e18edce864ec3cd679354e80217f4156bfb76
DavidBalazic <davidbalazic31@hotmail.com>
2025-01-15 16:26:24 +0100

    feat: changed to using DTOs

---
+++ FoodService/FoodService/DTOs/FoodDto.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodService.DTOs
+{
+    public class FoodDto
+    {
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Fat { get; set; }
+        public int ServingSize { get; set; }
+        public string Unit { get; set; }
+        public List<string> Allergens { get; set; }
+    }
+}
--- FoodService/FoodService/Services/FoodsService.cs
+++ FoodService/FoodService/Services/FoodsService.cs
@@ -1,4 +1,5 @@
-﻿using FoodService.Models;
+﻿using FoodService.DTOs;
+using FoodService.Models;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
@@ -24,7 +25,14 @@ namespace FoodService.Services
         public async Task<List<Food>> GetAllAsync() => await _foodsCollection.Find(_ => true).ToListAsync();
         public async Task<Food> GetAsync(string id) => await _foodsCollection.Find(p => p.Id == id).FirstOrDefaultAsync();
         public async Task<List<Food>> GetByMaxCaloriesAsync(double maxCalories) => await _foodsCollection.Find(f => f.Calories < maxCalories).ToListAsync();
-        public async Task<List<Food>> GetByExcludedAllergensAsync(List<string> excludedAllergens) => await _foodsCollection.Find(f => !f.Allergens.Any(allergen => excludedAllergens.Contains(allergen))).ToListAsync();
+        public async Task<List<Food>> GetByExcludedAllergensAsync(List<string> excludedAllergens)
+        {
+            var lowerCaseExcludedAllergens = excludedAllergens.Select(allergen => allergen.ToLower()).ToList();
+
+            return await _foodsCollection
+                .Find(f => !f.Allergens.Any(allergen => lowerCaseExcludedAllergens.Contains(allergen.ToLower())))
+                .ToListAsync();
+        }
         // POST
         public async Task CreateAsync(Food food) => await _foodsCollection.InsertOneAsync(food);
         public async Task CreateManyAsync(List<Food> foods) => await _foodsCollection.InsertManyAsync(foods);
@@ -34,7 +42,7 @@ namespace FoodService.Services
         public async Task DeleteAsync(string id) => await _foodsCollection.DeleteOneAsync(p => p.Id == id);
         public async Task<DeleteResult> DeleteByNameAsync(string name)
         {
-            return await _foodsCollection.DeleteManyAsync(f => f.Name.Contains(name));
+            return await _foodsCollection.DeleteManyAsync(f => f.Name == name);
         }
     }
 }
